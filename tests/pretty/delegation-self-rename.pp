e30299669e8d2d98cf2abc11b3063dbb656affdc
aerooneqq <aerooneq@yandex.ru>
2026-06-17 11:21:53 +0300

    Fix pretty tests

--- tests/pretty/delegation-self-rename.pp
+++ tests/pretty/delegation-self-rename.pp
@@ -19,10 +19,13 @@ impl <'a, A, const B: bool> Trait<'a, A, B> for X { }
 #[attr = Inline(Hint)]
 fn foo<'a, Self, A, const B: _, const B2: _, T, U,
     impl FnOnce() -> usize>(self: _, arg1: _) -> _ where
-    'a:'a { Trait::<'a, A, B>::foo::<B2, T, U>(self, arg1) }
+    'a:'a { <Self as Trait::<'a, A, B>>::foo::<B2, T, U>(self, arg1) }
 #[attr = Inline(Hint)]
 fn bar<Self, impl FnOnce() -> usize>(self: _, arg1: _)
-    -> _ { Trait::<'static, (), true>::foo::<true, (), ()>(self, arg1) }
+    ->
+        _ {
+    <Self as Trait::<'static, (), true>>::foo::<true, (), ()>(self, arg1)
+}
 
 #[attr = Inline(Hint)]
 fn foo2<'a, This, A, const B: _, const B2: _, T, U,
